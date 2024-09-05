c76727c6a7072eceeb4d202b2a518359a7c32bed
Abhijat Bhaumik <abhijatbhaumik@gmail.com>
2024-09-05 16:43:53 +0530

    First commit

---
+++ src/constants/index.js
@@ -0,0 +1,38 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+
+export const HERO_CONTENT = `As a front-end developer with a robust background in ReactJS, Tailwind, and MaterialUI, I am passionate about crafting seamless and visually compelling user interfaces. My expertise lies in translating complex requirements into intuitive and responsive designs, ensuring that each user interaction is smooth and engaging. With a strong commitment to best practices and continuous learning, I strive to stay at the forefront of the latest web technologies and trends. My ultimate goal is to provide exceptional user experiences that not only meet but exceed client expectations, making web applications both aesthetically pleasing and highly functional.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+
+export const PROJECTS = [
+  {
+    title: "Simon Game",
+    image: project1,
+    description:
+      "Developed an interactive Simon Game utilizing HTML, CSS, and JavaScript and implemented advanced jQuery techniques for dynamic gameplay and user interaction.",
+    technologies: ["HTML", "CSS", "JavaScript", "JQuery"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project2,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Tailwind"],
+  },
+  {
+    title: "YouTube Clone",
+    image: project3,
+    description:
+      "Developed a fully functional YouTube clone site using ReactJS and MaterialUI, incorporating API integration for real-time video content. The site features a sleek and intuitive user interface, providing a seamless user experience similar to the original platform.",
+    technologies: ["HTML", "CSS", "JavaScript", "ReactJs", "MaterialUI"],
+  },
+];
+
+export const CONTACT = {
+  address: "Kalpkriti Apartment, Risali, Bhilai ",
+  phoneNo: "+91 7024811012 ",
+  email: "abhijatbhaumik@gmail.com",
+};
